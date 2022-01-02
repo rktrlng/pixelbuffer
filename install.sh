f63e1545a49081f421703e949694d7d12af052ff
rktrlng <rik.teerling@gmail.com>
2022-01-02 21:52:46 +0100

    don't install headers

--- install.sh
+++ install.sh
@@ -7,9 +7,7 @@ if [ -n "$1" ]; then
 	rm -f /usr/local/bin/img2pbf
 	rm -f /usr/local/bin/pbf2img
 	rm -f /usr/local/bin/pbf2imgbulk
-	rm -rf /usr/local/lib/pixelbuffer
 else
 	# install
 	cp tools/* /usr/local/bin/
-	cp -r pixelbuffer /usr/local/lib/
 fi
